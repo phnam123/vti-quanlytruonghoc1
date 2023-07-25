f062228a1228aa24fd75c822486173c8a916f130
DESKTOP-OIJ636C\phanhoainam <phanhoainam12g4@gmail.com>
2023-07-25 14:53:16 +0700

    huuh

---
+++ src/main/java/com/vti/quanlytrghoc/QuanlytrghocApplication.java
@@ -0,0 +1,13 @@
+package com.vti.quanlytrghoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlytrghocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlytrghocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vti/quanlytrghoc/QuanlytrghocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.quanlytrghoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlytrghocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
